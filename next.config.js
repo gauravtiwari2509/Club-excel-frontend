4046584166d1fa879baaed94e0a8bc62be6a6a4e
abhinavnist <abhinavpatna55@gmail.com>
2023-12-20 02:15:37 +0530

    changes by fault

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- 
   compiler: {
     styledComponents: true,
   },
-  
-};
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- pages/test.js
+++ pages/test.js
@@ -1,54 +1,15 @@
-<<<<<<< HEAD
-import Particles from "@/components/Particles"
-import VideoAnimation from "@/components/VideoAnimation"
-import Form from "@/components/form"
-import React from "react"
-import styled from "styled-components"
-
-const MainCont = styled.div`
-
-
-
-
-
-`
-
-function Navbar() {
-  return <MainCont>
-
-
-
-  
-  </MainCont>
-}
-
-export default Navbar
-=======
 import Particles from "@/components/Particles"
 import AdvisorCard from "@/components/Team/Advisor"
 import VideoAnimation from "@/components/VideoAnimation"
 import Form from "@/components/form"
 import React, { useEffect, useState } from "react"
 import styled from "styled-components"
-import { useInView } from 'react-intersection-observer';
+import { useInView } from "react-intersection-observer"
 
-const MainCont = styled.div`
-
-
-
-
-`
+const MainCont = styled.div``
 
 function Navbar() {
-
-  
-
-  return <MainCont>
-
-  
-
-  </MainCont>
+  return <MainCont></MainCont>
 }
 
 export default Navbar
->>>>>>> 4aaae9bd46d8f8c5fed4ddffbbe1fc9f274de6ee
